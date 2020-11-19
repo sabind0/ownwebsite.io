2d9bfd54f3899bc9d10c98607fbea962f634fc87
unknown <aryaldivash91@gmail.com>
2020-11-19 21:04:59 +0545

    today

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.button').click(function(){
+        $('nav').toggleClass('active')
+    });
+var typed = new Typed(".typing", {
+        strings: ["Student", "Frontend-Developer", "Graphic Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+ });
